ae4fd967c0031c99c80e992b3a66b544098264cc
Analia Mok <analiam319@gmail.com>
2019-05-05 14:36:31 -0400

    Stubbed out dynamic roasters route link

--- src/router.ts
+++ src/router.ts
@@ -22,6 +22,10 @@ export default new Router({
       path: 'roasters',
       name: 'roasters',
       component: () => import('./views/roasters/RoastersList.vue')
+    },
+    {
+      path: 'roasters/:roasterId',
+      component: () => import('./views/roasters/RoasterDetail.vue')
     }
   ]
 });
